8edc4672452f5bfad10bed56b43e3fa0f27b6b3f
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2022-10-08 01:30:39 +0900

    [Silver I] Title: 안전 영역, Time: 304 ms, Memory: 113660 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -45,16 +45,17 @@ public class Main {
 			}//for j
 		}//for i
 		
+		//최소높이부터 최대높이까지 안전영역을 탐색한다.
 		int maxArea=Integer.MIN_VALUE;
 		for(int height=min; height<max; height++) {
 			visited = new boolean[N+2][N+2];
 			int cnt=0;
+			
+			//1,1 부터 N,N까지 BFS 탐색하여 안전영역 갯수 구하기
 			for(int i=1; i<=N; i++) {
 				for(int j=1; j<=N; j++) {
-					if(!visited[i][j]) {
-						if(bfs(new Position(i,j),height)) {
-							cnt++;
-						}//if
+					if(bfs(new Position(i,j),height)) { //탐색하는지 검사
+						cnt++;
 					}//if
 				}//for j
 			}//for i
@@ -65,9 +66,11 @@ public class Main {
 	}
 
 	private static boolean bfs(Position start, int height) {
+		//시작점이 이미 방문했거나 잠겨있으면 탐색 종료
 		if(visited[start.r][start.c] || map[start.r][start.c]<=height) 
 			return false;
 		
+		//탐색 시작 (무조건 true 반환)
 		Queue<Position> que = new ArrayDeque<>();
 		que.offer(start);
 		visited[start.r][start.c] = true;
@@ -76,9 +79,9 @@ public class Main {
 			Position cur = que.poll();
 			for(int i=0; i<4; i++) {
 				Position next = new Position(cur.r+dr[i], cur.c+dc[i]);
-				if(next.isOut()) continue;
-				if(visited[next.r][next.c]) continue;
-				if(map[next.r][next.c]<=height) continue;
+				if(next.isOut()
+				 ||visited[next.r][next.c]
+				 ||map[next.r][next.c]<=height) continue;
 				visited[next.r][next.c] = true;
 				que.offer(next);
 			}
