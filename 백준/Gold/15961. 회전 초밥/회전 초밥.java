57a72c16ef478761c9ba15fffc59e7367a501ea0
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2022-10-11 23:03:52 +0900

    [Gold IV] Title: 회전 초밥, Time: 624 ms, Memory: 212600 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -16,35 +16,42 @@ public class Main {
 		int maxValue = Integer.parseInt(st.nextToken());
 		int streakSize = Integer.parseInt(st.nextToken());
 		int couponValue = Integer.parseInt(st.nextToken());
-		int[] values = new int[size];
-		int[] valueCnts = new int[maxValue+1];
-		int sameCntMax = Integer.MIN_VALUE;
+		int[] values = new int[size];			//스시 입력받을 배열
+		int[] valueCnts = new int[maxValue+1];	//스시 종류 별로 큐에 몇개 들어있는지 저장할 배열
+		int sameCntMax = Integer.MIN_VALUE;		//스시 가짓수 최대 갯수
+		int sameCnt=0;							//스시 가짓수 카운트
 		
 		Queue<Integer> que = new ArrayDeque<>();
 		Set<Integer> set = new HashSet<>();
+		
+		
 		
 		for(int i=0; i<size; i++) {
 			values[i] = Integer.parseInt(br.readLine());
 		}
 		
 		for(int i=0; i<size+streakSize; i++) {
-			int idx = i%size;
-			que.offer(values[idx]); //큐에 추가
+			int insVal = values[i%size];
 			
-			valueCnts[ values[idx] ]++;
-			set.add(values[idx]);
+			que.offer(insVal); //큐에 추가
+			
+			valueCnts[ insVal ]++;
+			if(valueCnts[ insVal ] == 1) {//값을 추가하여 한개라면
+				sameCnt++;
+			}
+			//set.add(values[idx]);
 
 			if(que.size()>streakSize) { //연속으로 담을 수 있는 최대 크기라면
 				int delVal = que.poll(); //큐에서 제거
 				valueCnts[ delVal ]--;
 				if(valueCnts[ delVal ] == 0) {//값을 삭제하여 한개도 없다면
-					set.remove(delVal);
+					sameCnt--;
 				}
 				
-				if(set.contains(couponValue)) { //set에 쿠폰 스시가 있다면
-					sameCntMax = Math.max(sameCntMax, set.size()); //셋 크기 그대로 비교
-				} else {
-					sameCntMax = Math.max(sameCntMax, set.size()+1); //셋크기+1과 비교
+				if(valueCnts[ couponValue ]>0) { //que에 쿠폰 스시가 있다면
+					sameCntMax = Math.max(sameCntMax, sameCnt); //가짓수 카운트 그대로 비교
+				} else { //없다면
+					sameCntMax = Math.max(sameCntMax, sameCnt+1); //가짓수 카운트+1과 비교
 				}
 			}
 		}
