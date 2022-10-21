4102a4f5f4ecbd4eba59b6f99eeb04ad8cd2e9b1
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2022-10-22 00:02:33 +0900

    [Gold V] Title: 합분해, Time: 108 ms, Memory: 12076 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,30 +1,58 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] s = br.readLine().split(" ");
-		int n = Integer.parseInt(s[0]);
-		int k = Integer.parseInt(s[1]);
-		long[][] dp = new long[k+1][n+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		final long MOD = 1000000000; 
 		
-		for(int i=0; i<dp.length; i++) {
-			dp[i][0]=1;
-		}
-		for(int i=0; i<dp[1].length; i++) {
-			dp[1][i]=1;
-		}
+		long[][] arr = new long[n+1][k+1];
 		
-		for (int i = 2; i <= k; i++) {
-			for (int j = 1; j <= n; j++) {
-				dp[i][j] = dp[i-1][j] + dp[i][j-1];
-				dp[i][j] %= 1000000000;
+		// arr[n][1]은 항상 1임
+		for(int i=1; i<=n; i++) {
+			arr[i][1] = 1;
+		}
+		// arr[0][1~k] 은 항상 1임
+		for(int i=1; i<=k; i++) {
+			arr[0][i] = 1;
+		}
+		if(k>1) {
+			//k가 2 이상일때만 아래가 수행되야함
+			for(int i=1; i<=n; i++) {
+				for(int j=2; j<=k; j++) {
+					for(int l=0; l<=i; l++) { 
+						arr[i][j] += arr[l][j-1];
+						arr[i][j] %= MOD;
+					}
+				}
 			}
 		}
-		
-		System.out.println(dp[k][n]);
+//		printArr(arr, n, k);
+//		System.out.println();
+		System.out.println((arr[n][k]));
+	}
+
+	private static void printArr(long[][] arr, int n, int k) {
+		for(int i=0; i<=n; i++) {
+			System.out.println("N="+i);
+			for(int j=0; j<=k; j++) {
+				System.out.printf("  arr[%d][%d] : %d%n",i,j,arr[i][j]);
+			}
+		}
+	}
+	
+	private static void printArr(BigInteger[][] big, int n, int k) {
+		for(int i=0; i<=n; i++) {
+			System.out.println("N="+i);
+			for(int j=0; j<=k; j++) {
+				System.out.printf("  big[%s][%s] : %d%n",i,j,big[i][j]);
+			}
+		}
 	}
 }
