3520184d660f4153a8604b0eb099e02254181cf2
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-03-18 11:39:08 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,11 @@
+# 1. 2022년 10월에 작성
+# 2. 댓글 작성일 오름차순, 게시글 제목 오름차순
+# 3. 
+# 3. 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일
+
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, DATE_FORMAT(r.CREATED_DATE, "%Y-%m-%d") AS CREATED_DATE
+FROM USED_GOODS_BOARD AS b
+JOIN USED_GOODS_REPLY AS r
+    ON b.BOARD_ID = r.BOARD_ID
+WHERE DATE_FORMAT(b.CREATED_DATE, "%Y-%m") = "2022-10"
+ORDER BY CREATED_DATE ASC, b.TITLE ASC;
