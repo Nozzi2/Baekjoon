8b48a533b01b09376f5026e62d106dabf75b58a6
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-07-02 12:35:08 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,66 +1,75 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+1번 컴퓨터에 연결된 노드를 찾아서,
+결과를 출력하면 됨.
+
+단, 사이클이 있을 때 무한으로 돌지 않도록 처리를 해줘야함.
+
+컴퓨터 수는 최대 100개
+연결되어있는 배열을 선언해서 BFS 탐색 진행하면 될듯
+
+1. 2차원 배열에 양방향 연결헌다.
+2. BFS탐색을 진행하는데,
+    1번 컴퓨터에 연결된 컴퓨터를 que에 담고
+    1번 컴퓨터는 방문체크 해주고
+    바이러스 감염 갯수++
+3. que에 담겨있는 컴퓨터에 연결된 컴퓨터를 검사해서
+    방문한적이 없는지?
+        que에 담고
+        방문체크
+        감염갯수 ++
+4. que가 빌 때 까지
+ */
 public class Main {
 
-	static int[] parents;
-	static int V;
-	
-	static void make() {
-		parents = new int[V];
-		for(int i=0; i<V; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	static int find(int a) {
-		if(parents[a] == a) {
-			return a;
-		}
-		
-		return parents[a] = find(parents[a]);
-	}
-	
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		
-		if(aRoot == bRoot) {
-			return false;
-		}
-		
-		parents[aRoot] = bRoot;
-		return true;
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		V=Integer.parseInt(br.readLine());
-		make();
-		
-		int TC = Integer.parseInt(br.readLine());
-		for(int T=0; T<TC; T++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken())-1;
-			int b = Integer.parseInt(st.nextToken())-1;
-			union(a,b);
-		}
-		
-		int cnt=0;
-		int p = find(0);
-		for(int i=1; i<V; i++) {
-			int out = find(i);
-			//System.out.println((i+1)+" : "+(out+1));
-			if(out==p) {
-				cnt++;
-			}
-		}
-		
-		System.out.println(cnt);
-		
-	}
+    static boolean[][] matrix;
+    static boolean[] isVisited;
+    static int count;
+    static int size;
 
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        size = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
+        count=0;
+        matrix = new boolean[size][size];
+        isVisited = new boolean[size];
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken())-1;
+            int e = Integer.parseInt(st.nextToken())-1;
+            matrix[s][e] = true;
+            matrix[e][s] = true;
+        }
+
+        bfs(0);
+
+        System.out.println(count);
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> que = new ArrayDeque<>();
+        que.offer(start);
+        isVisited[start] = true;
+
+        while (!que.isEmpty()) {
+            int next = que.poll();
+//            System.out.println("??");
+            for (int i = 1; i < size; i++) {
+                if (!matrix[next][i] || isVisited[i]) continue;
+                count++;
+                que.offer(i);
+                isVisited[i] = true;
+            }
+        }
+    }
 }
