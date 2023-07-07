1d246c0d97c4a05fcf85e2204bdf91117a4ef78a
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-07-07 19:24:53 +0900

    [Gold III] Title: 내리막 길, Time: 404 ms, Memory: 38100 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,123 +1,107 @@
+/*
+내리막길로만 내려가서 가장 오른쪽 아래칸으로 도착하는 '모든 경우의 수'를 출력해야하므로
+DFS로 탐색하면 된다.
+
+DFS 탐색할때 다음 탐색할 위치가 현재 위치보다 숫자가 작은지 여부 검사해서
+계속 재귀 호출해주면 된다!
+
+재귀 호출할때는 호출 후 검사하는 것 보다
+호출 전 검사하여 백트래킹하는 것이 훨씬 더 메모리를 줄일 수 있다.
+ */
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[] dr = {-1, 1, 0, 0};
-	static int[] dc = {0, 0, -1, 1};
-	static int R,C,cnt;
-	static int[][] map;
-	static int[][] memo;
-	
-	static class Position {
-		int r;
-		int c;
-		
-		public Position(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-		
-		boolean isOut() {
-			if(r>R || c>C || r<1 || c<1) {
-				return true;
-			}
-			return false;
-		}
-		
-		boolean isArrived() {
-			if(r==R && c==C) {
-				return true;
-			}
-			return false;
-		}
-		
-		boolean higherThan(Position p) {
-			if(map[r][c] > map[p.r][p.c]) {
-				return true;
-			}
-			return false;
-		}
-	}
+    static int[][] matrix;
+    static int[][] memo;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, -1, 0, 1};
+    static int count, sizeR, sizeC;
 
-	//구글링해서 풀이법 보고 구현함
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		map = new int[R+2][C+2];
-		memo = new int[R+2][C+2];
-		
-		for(int i=1; i<=R; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=C; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				memo[i][j] = -1; //방문하지 않았음 처리
-			}
-		}
-		
-		Position start = new Position(1,1);
-		
-		System.out.println(dfs(start));
-	}
-	
-	
-	//메모리초과되어 메모이제이션 추가
-	/*
-	 * 방문여부를 체크하고, 방문한 곳을 탐색하여 도착지에 도달한다면 +1을 리턴받아서 해당 경로의 도착 경우의 수를 저장한다.
-	 * 상하좌우로 모두 탐색하기 때문에 모든 경우의 수를 고려할 수 있고,
-	 * 방문하는 곳은 단 한번만 방문하여 불필요한 연산을 줄일 수 있다. 
-	 * 최종적으로는 시작점에 모든 방문가능한 경우의 수가 저장된다.
-	 * ex) 4 3	 >	2 1
-	 *     2 1		1 -1
-	 */
-	private static int dfs(Position start) {
-		if(start.isArrived()) {
-			return 1;
-		}
-		
-		if(memo[start.r][start.c] != -1) { //방문했었다면
-			return memo[start.r][start.c];
-		} else { //방문하지 않았다면
-			memo[start.r][start.c]=0; //일단 방문 처리
-			for(int i=0; i<4; i++) { //해당칸의 모든 경로의 경우의 수를 더하기 위해 반복
-				Position next = new Position(start.r+dr[i], start.c+dc[i]);
-				
-				if(next.isOut()) continue;
-				if(start.higherThan(next)) {
-					//다음 방문할 곳에 도착 경로가 몇개인지 저장
-					memo[start.r][start.c] += dfs(next); 
-				}
-			}
-		}
-		
-		return memo[start.r][start.c];
-	}
+    static class Pos {
+        int r;
+        int c;
+        int cost;
 
-	//메모리초과
-	private static void bfs(Position start) {
-		Queue<Position> que = new ArrayDeque<>();
-		que.offer(start);
-		
-		while(!que.isEmpty()) {
-			Position cur = que.poll();
-			for(int i=0; i<4; i++) {
-				Position next = new Position(cur.r+dr[i], cur.c+dc[i]);
-				if(next.isOut()) continue;
-				if(!cur.higherThan(next)) continue; //현재블럭이 다음 블럭보다 높지 않으면
-				if(next.isArrived()) {
-					cnt++;
-					continue;
-				}
-				que.offer(next);
-			}
-		}
-	}
+        public Pos(int r, int c) {
+            this.r = r;
+            this.c = c;
+            this.cost = matrix[r][c];
+        }
 
+        public Pos(Pos p, int d) {
+            this.r = p.r+dr[d];
+            this.c = p.c+dc[d];
+            this.cost = matrix[r][c];
+        }
+
+        //영역을 벗어났는지 검사
+        public boolean isOut() {
+            return r < 1 || r > sizeR || c < 1 || c > sizeC;
+        }
+
+        //도착했는지 여부 검사
+        public boolean isArrived() {
+            return r == sizeR && c == sizeC;
+        }
+
+        public boolean isChecked() {
+            return memo[r][c] != -1;
+        }
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sizeR = Integer.parseInt(st.nextToken());
+        sizeC = Integer.parseInt(st.nextToken());
+        matrix = new int[sizeR + 2][sizeC + 2];
+        memo = new int[sizeR + 2][sizeC + 2];
+        count = 0;
+
+        for (int i = 1; i <= sizeR; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= sizeC; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+                memo[i][j] = -1;
+            }
+        }
+
+        Pos start = new Pos(1,1);
+
+        System.out.println(dfs(start));
+    }
+
+    static int dfs(Pos cur) {
+        if(cur.isArrived()){
+            return 1;
+        }
+
+        //이미 방문했었다면
+        if(cur.isChecked()){
+            return memo[cur.r][cur.c];
+        }
+
+        memo[cur.r][cur.c] = 0; //방문처리
+
+        for (int i = 0; i < 4; i++) {
+            Pos next = new Pos(cur, i);
+            if(next.isOut()) continue;
+            if(!isDown(cur, next)) continue;
+
+            memo[cur.r][cur.c] += dfs(next);
+        }
+
+        return memo[cur.r][cur.c];
+    }
+
+    static boolean isDown(Pos cur, Pos next) {
+        return cur.cost > next.cost;
+    }
 
 }
