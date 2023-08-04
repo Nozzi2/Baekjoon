4f1817516a3cb76f7018fedb815094ecafc469d7
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-08-05 00:57:05 +0900

    [Gold V] Title: 집합의 표현, Time: 664 ms, Memory: 63744 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,71 +1,76 @@
-/*
-Disjoin set == 서로소 집합 == Union find 의 가장 대표적인 문제이다.
-순전히 Union find 알고리즘 만으로 풀 수 있는 문제임.
+import java.util.*;
+import java.io.*;
 
-먼저 union, find, isSameParent 메소드 부터 구현하여 실습해보고
-문제에서 주어지는 요구사항대로 출력할 수 있게 바꿔보자
- */
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class Main {
+public class Main
+{
+    static int[] arr;
 
-    static int[] parents;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws Exception
+    {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int size = Integer.parseInt(st.nextToken());
-        parents = new int[size+1];
-        for (int i = 1; i <= size; i++) {
-            parents[i] = i;
-        }
-        int[] clone = parents.clone();
+        int TCASE = Integer.parseInt(st.nextToken());
 
-        int T = Integer.parseInt(st.nextToken());
-        for (int t = 0; t < T; t++) {
+        arr = new int[size+1];
+        
+        for(int i=1; i<=size; i++){
+            arr[i] = i;
+        }
+
+        for(int T=0; T<TCASE; T++){
             st = new StringTokenizer(br.readLine());
             int command = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
 
-//            int A = find(a);
-//            int B = find(b);
-
-            if (command == 0) {
-                union(a,b);
-            } else if (command == 1) {
-                System.out.println(isSameParent(a,b)?"YES":"NO");
+            if(command==0){
+                //union
+                union(num1, num2);
+            } else {
+                //isSameParent
+                if(isSameParent(num1, num2)){
+                    sb.append("YES\n");
+                } else {
+                    sb.append("NO\n");
+                }
             }
-//            System.out.println(Arrays.toString(clone));
-//            System.out.println(Arrays.toString(parents));
-//            System.out.println("---------------------------");
         }
+
+        System.out.print(sb.toString());
     }
 
-    static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if(a < b) {
-            parents[b] = a;
-        } else {
-            parents[a] = b;
-        }
-    }
-
-    static int find(int a) {
-        if (parents[a] == a) return a;
-        return parents[a] = find(parents[a]);
-    }
-
-    static boolean isSameParent(int a, int b) {
+    static void union(int a, int b){
         int parentA = find(a);
         int parentB = find(b);
 
-        return parentA == parentB;
+        if(parentA < parentB){
+            arr[parentB] = parentA;
+        } else {
+            arr[parentA] = parentB;
+        }
+    }
+
+    static int find(int a){
+        if(a == arr[a]){
+            return a;
+        } else {
+            return arr[a] = find(arr[a]);
+        }
+    }
+
+    static boolean isSameParent(int a, int b){
+
+        int parentA = find(a);
+        int parentB = find(b);
+
+        if(parentA == parentB) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
