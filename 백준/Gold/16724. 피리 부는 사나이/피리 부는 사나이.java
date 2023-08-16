4a7087b97ddf1e3a33ce930efca13542829a9f49
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-08-16 18:12:51 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 368 ms, Memory: 50084 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -1,115 +1,344 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
+/*
+2차원 배열의 칸별로 방향이 주어지고 해당 방향에 따라 배열을 이동함.
+더이상 이동하지 못하게 하는 칸을 최소의 갯수로 만든다고 함.
+모든 칸에서 이동을 시작하여 더이상 이동하지 못하게 하는 칸으로 도착하게 하기위해
+만들어야하는 칸의 갯수는?
+
+방향대로 움직이면서 방문체크를 해준다.
+방문체크는 int값으로 하는데,
+이 int값은 몇번째 탐색인지 저장하는 값임.
+만약 이미 방문체크 되어있는 칸에 도착한다면
+    저장되어 있는 값이 현재 탐색과 같다면
+        만들어야하는 칸 갯수++
+아니라면
+    이미 방문했었으므로 다음꺼 탐색
+ */
 public class Main {
 
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-    static int R,C;
-    static List<Pos> list;
-    static int[][] map;
-    static int[][] visited;
+    static int[][] rounds;
+    static char[][] matrix;
+    static int sizeR, sizeC;
 
-    static class Pos {
+    static class Man {
         int r;
         int c;
+        int round;
 
-        public Pos(int r, int c){
+        public Man(int r, int c, int round) {
             this.r = r;
             this.c = c;
+            this.round = round;
         }
 
-        @Override
-        public String toString() {
-            return "Pos{" +
-                    "r=" + r +
-                    ", c=" + c +
-                    '}';
+        void move() {
+            switch (matrix[r][c]) {
+                case 'D' :
+                    r++;
+                    break;
+                case 'U' :
+                    r--;
+                    break;
+                case 'L' :
+                    c--;
+                    break;
+                case 'R' :
+                    c++;
+                    break;
+            }
+        }
+
+        boolean isOut() {
+            return r < 1 || r > sizeR || c < 1 || c > sizeC;
+        }
+
+        boolean isThisRoundVisited() {
+            return rounds[r][c] == this.round;
+        }
+
+        boolean isVisited() {
+            return rounds[r][c] != 0;
+        }
+
+        void setVisit() {
+            rounds[r][c] = this.round;
         }
     }
 
     public static void main(String[] args) throws Exception {
-        list = new ArrayList<Pos>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        int szCnt=0;
-        map = new int[R+2][C+2];
-        visited = new int[R + 2][C + 2]; //1일경우 방문O, 2일경우 방문O & 안전지대 카운트 O
-        for(int i=1; i<=R; i++){
-            char[] chars = br.readLine().toCharArray();
-            for(int j=1; j<=C; j++){
-                map[i][j] = getDir(chars[j - 1]);
+        sizeR = Integer.parseInt(st.nextToken());
+        sizeC = Integer.parseInt(st.nextToken());
+        matrix = new char[sizeR + 2][sizeC + 2];
+        rounds = new int[sizeR + 2][sizeC + 2];
+
+        for (int r = 1; r <= sizeR; r++) {
+            char[] input = br.readLine().toCharArray();
+            for (int c = 1; c <= sizeC; c++) {
+                matrix[r][c] = input[c - 1];
             }
         }
 
-        //1,1부터 r,c까지 탐색
-        for(int i=1; i<=R; i++){
-            for(int j=1; j<=C; j++){
-                if(visited[i][j]==0){//탐색하지 않았었으면 탐색 진행
-                    Pos safeZone = checkMap(new Pos(i, j));
-                    //탐색 마친 후 도착한 곳이 이전에 방문하지 않았다면
-                    if(visited[safeZone.r][safeZone.c]==1){
-                        szCnt++; //안전지대 갯수 ++
-//                        System.out.println(safeZone);
-                    }
-//                    for(int a=0; a<=R+1; a++){
-//                        for(int b=0; b<=C+1; b++){
-//                            System.out.print(visited[a][b]+" ");
+        int round = 0;
+        int count = 0;
+        for (int r = 1; r <= sizeR; r++) {
+            for (int c = 1; c <= sizeC; c++) {
+                if (rounds[r][c] != 0) continue; //이전에 방문한적이 있다면 종료
+
+                Man man = new Man(r, c, ++round);
+                while (true) {
+
+//                    for (int r1 = 1; r1 <= sizeR; r1++) {
+//                        for (int c1 = 1; c1 <= sizeC; c1++) {
+//                            System.out.print(man.r==r1 && man.c==c1?"# ":"- ");
 //                        }
 //                        System.out.println();
 //                    }
-//                    System.out.println(list.size());
 //                    System.out.println();
-                    //이번 탐색에 방문한 모든 칸 값 갱신
-                    for(Pos p : list){
-                        visited[p.r][p.c]=2;
+
+                    man.setVisit();
+                    man.move();
+                    if(man.isOut()) {
+                        count++;
+                        break;
                     }
-                    list.clear();
+                    if(man.isVisited()) {
+                        if (man.isThisRoundVisited()) {
+                            count++;
+                        }
+                        break;
+                    }
                 }
+//                System.out.println("---------------------");
             }
         }
 
-//        for(int i=0; i<=R+1; i++){
-//            for(int j=0; j<=C+1; j++){
-//                System.out.print(map[i][j]+" ");
+//        for (int r = 1; r <= sizeR; r++) {
+//            for (int c = 1; c <= sizeC; c++) {
+//                System.out.print(rounds[r][c]+" ");
 //            }
 //            System.out.println();
 //        }
 
-        System.out.println(szCnt);
-    }
-
-    private static Pos checkMap(Pos cur) {
-//        System.out.println("  탐색 시작했다~");
-        //방문한 칸이 나올때까지 방향대로 탐색 진행
-        while(visited[cur.r][cur.c]==0){
-            list.add(new Pos(cur.r,cur.c));
-            visited[cur.r][cur.c]=1;
-            int dir = map[cur.r][cur.c];
-//            System.out.println("    "+dir+"방향 / "+cur.r+","+cur.c);
-            cur.r += dr[dir];
-            cur.c += dc[dir];
-        }
-//        System.out.println("  탐색 끝났다~");
-        return cur;
-    }
-
-    private static int getDir(char c){
-        switch (c){
-            case 'U':
-                return 0;
-            case 'D':
-                return 1;
-            case 'L':
-                return 2;
-            case 'R':
-                return 3;
-        }
-        return -1;
+        System.out.println(count);
     }
 }
+/*
+3 4
+DLLL
+DRLU
+RRRU
+
+5 6
+DRRDLL
+DURDDL
+DUULLU
+RRRRDU
+UUUURR
+
+# - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+- - - - - -
+# - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+# - - - - -
+- - - - - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - - - - -
+# - - - - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - - - - -
+- # - - - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - - - - -
+- - # - - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - - - - -
+- - - # - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - # -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - # -
+
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - #
+
+---------------------
+- # - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+- - # - - -
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+- - - # - -
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+- - - - - -
+- - - # - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - - # - -
+- - - - - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - # - - -
+- - - - - -
+- - - - - -
+
+- - - - - -
+- - # - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+---------------------
+- - - - # -
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+---------------------
+- - - - - #
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+---------------------
+- - - - - -
+- # - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+
+---------------------
+- - - - - -
+- - - - # -
+- - - - - -
+- - - - - -
+- - - - - -
+
+- - - - - -
+- - - - - -
+- - - - # -
+- - - - - -
+- - - - - -
+
+---------------------
+- - - - - -
+- - - - - #
+- - - - - -
+- - - - - -
+- - - - - -
+
+---------------------
+- - - - - -
+- - - - - -
+- # - - - -
+- - - - - -
+- - - - - -
+
+---------------------
+- - - - - -
+- - - - - -
+- - - - - #
+- - - - - -
+- - - - - -
+
+---------------------
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - #
+- - - - - -
+
+---------------------
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+# - - - - -
+
+---------------------
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+- # - - - -
+
+---------------------
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+- - # - - -
+
+---------------------
+- - - - - -
+- - - - - -
+- - - - - -
+- - - - - -
+- - - # - -
+
+---------------------
+1 2 2 2 3 4
+1 5 2 2 6 7
+1 8 2 2 6 9
+1 1 1 1 1 10
+11 12 13 14 1 1
+2
+
+ */
