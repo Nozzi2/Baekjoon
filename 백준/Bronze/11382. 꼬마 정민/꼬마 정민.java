58ebf14391025cb4cfbae21255b9c23b767a1b86
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-09-02 23:58:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        long result = a+b+c;
+        System.out.println(result);
+    }
+}
+
+// 000 000 000 000
