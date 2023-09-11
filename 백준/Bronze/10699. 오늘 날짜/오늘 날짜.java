222bcebeba81429ce7a5979d94a876e862ba1851
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-09-12 00:17:36 +0900

    [Bronze V] Title: 오늘 날짜, Time: 268 ms, Memory: 18760 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.text.*;
+import java.time.*;
+
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Date date = new Date();
+		DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
+		df.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println(df.format(date));
+    }
+}
