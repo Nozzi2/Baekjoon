f749a5e176999157ed7d5560041f795477d812e0
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-10-06 19:45:17 +0800

    [Bronze III] Title: 택시 기하학, Time: 132 ms, Memory: 14600 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		double R = Double.parseDouble(br.readLine());
+		
+		System.out.println(String.format("%.6f", (Math.pow(R, 2) * Math.PI)));
+		System.out.println(String.format("%.6f", (2 * Math.pow(R, 2))));
+	}
+
+}
