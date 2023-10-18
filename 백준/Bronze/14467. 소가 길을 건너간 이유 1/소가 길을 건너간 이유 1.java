efd3544b5b5e595fd69a5aa436443837386e22c5
Nozzi2 <101002050+Nozzi2@users.noreply.github.com>
2023-10-18 14:15:24 +0800

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
@@ -1,28 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static int[] cow;
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int answer=0;
-		n=sc.nextInt();
-		cow=new int[11];
-		Arrays.fill(cow, -1);
-		
-		for (int i = 0; i < n; i++) {
-			int num=sc.nextInt();
-			int pos=sc.nextInt();
-			if(cow[num]==-1) {
-				cow[num]=pos;
-			}else if(cow[num]!=pos) {
-				cow[num]=pos;
-				answer+=1;
-			}
-		}
-		
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int reqSize = Integer.parseInt(br.readLine());
+        int[] cows = new int[11];
+        final int INIT = -1;
+        Arrays.fill(cows, INIT);
 
-		System.out.println(answer);
-	}
+        StringTokenizer st;
+        int count = 0;
+        for (int i = 0; i < reqSize; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cowIdx = Integer.parseInt(st.nextToken());
+            int road = Integer.parseInt(st.nextToken());
+            if (cows[cowIdx] == INIT) {
+                cows[cowIdx] = road;
+                continue;
+            }
+            if (cows[cowIdx] != road) {
+                count++;
+                cows[cowIdx] = road;
+            }
+        }
+
+        System.out.println(count);
+    }
 }
+/*
+8
+3 1
+3 0
+6 0
+2 1
+4 1
+3 0
+4 0
+3 1
+
+ */
